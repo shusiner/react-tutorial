ae9010356c52791472a50b7415b5b89a0443b1d6
ng huan ran <quantum.plato@gmail.com>
2020-07-31 20:54:22 +0800

    Updates

---
+++ precache-manifest.af9c51309d71098eec3bdc4258eba647.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0d3d6a752a8ab21b7fed469f035e4e",
+    "url": "/shusiner.github.io/index.html"
+  },
+  {
+    "revision": "7081f22ae0d01f249d84",
+    "url": "/shusiner.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7abe38b47505c42d57d2",
+    "url": "/shusiner.github.io/static/js/2.c54a9235.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shusiner.github.io/static/js/2.c54a9235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7081f22ae0d01f249d84",
+    "url": "/shusiner.github.io/static/js/main.12002ea4.chunk.js"
+  },
+  {
+    "revision": "8bf0b525159031ac5ca7",
+    "url": "/shusiner.github.io/static/js/runtime-main.f84c1df9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shusiner.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
